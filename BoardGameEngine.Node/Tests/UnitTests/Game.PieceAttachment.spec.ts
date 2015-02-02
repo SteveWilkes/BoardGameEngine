b57a209cf8cf9fe46a2ddd0352ce5057af4e48f0
Steve Wilkes <steve@agileobjects.co.uk>
2015-02-02 21:27:01 +0000

    More piece attachment tests

--- BoardGameEngine.Node/Tests/UnitTests/Game.PieceAttachment.spec.ts
+++ BoardGameEngine.Node/Tests/UnitTests/Game.PieceAttachment.spec.ts
@@ -9,6 +9,36 @@ var gameBuilder: Ut.IGameBuilderService = require("./Game.GameBuilder");
 describe("Game", () => {
     describe("Piece attachment", () => {
 
+        it("Should exclude attachment targets by piece definition id", () => {
+            var game = gameBuilder.createGame(gc => gc
+                .withA3x3NorthSouthBoard()
+                .withHumanLocalAndRemotePlayers()
+                .withATeamForPlayer(1, tc => tc
+                    .withAPieceAt(["1x1"], pc => pc
+                        .withUdlrMovementBy(1)
+                        .withUdlrAttachmentTo(["3"]))
+                    .withAPieceAt(["1x2"], pc => pc
+                        .withUdlrMovementBy(1))
+                    .withAPieceAt(["1x3"], pc => pc
+                        .withUdlrMovementBy(1))));
+
+            var pieces = TsNs.Joq.select(game.teams[0].getPieces(), (p: AgileObjects.BoardGameEngine.Pieces.Piece) => p).toArray();
+            expect(pieces.length).toBe(3);
+
+            var piece = pieces[0];
+            var pieceInteractions = piece.interactionProfile.getPotentialInteractions(piece, game);
+
+            var attachmentInteraction = TsNs.Joq
+                .select(pieceInteractions, (inter: IPieceInteraction) => inter)
+                .where((inter: IPieceInteraction) => inter.location.coordinates.signature === "1x2")
+                .firstOrDefault();
+
+            // The piece at 1x1 can move to 1x2 but only attaches 
+            // to the type of piece at 1x3, so there shouldn't be 
+            // an attachment interaction:
+            expect(attachmentInteraction).toBeNull();
+        });
+
         it("Should attach one piece to another", () => {
             var game = gameBuilder.createGame(gc => gc
                 .withA3x3NorthSouthBoard()
