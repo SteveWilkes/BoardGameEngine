ef67495c5876b69c5b6ba87c0ad632e005bc1bce
Steve Wilkes <steve@agileobjects.co.uk>
2015-02-10 20:05:08 +0000

    PropertyValueEvaluator with multiple options parsing test

--- BoardGameEngine.Node/Tests/UnitTests/TypeScript.Evaluation.Parsing.spec.ts
+++ BoardGameEngine.Node/Tests/UnitTests/TypeScript.Evaluation.Parsing.spec.ts
@@ -4,13 +4,26 @@ var TsNs = Ao.TypeScript;
 
 describe("TypeScript", () => {
     describe("Evaluation", () => {
-        it("Should parse a PropertyEvaluator", () => {
+        it("Should parse a PropertyEvaluator with a single option", () => {
             var propertyName = "test";
             var propertyValue = "123";
             var item = {};
             item[propertyName] = propertyValue;
 
             var pattern = "pe{" + propertyName + ",[" + propertyValue + "]}";
+            var evaluator = TsNs.Evaluation.EvaluatorParser.INSTANCE.parse(pattern);
+            var result = evaluator.evaluate(item);
+
+            expect(result).toBeTruthy();
+        });
+
+        it("Should parse a PropertyEvaluator with multiple options", () => {
+            var propertyName = "test";
+            var propertyValue = "123";
+            var item = {};
+            item[propertyName] = propertyValue;
+
+            var pattern = "pe{" + propertyName + ",[hello," + propertyValue + ",lalala]}";
             var evaluator = TsNs.Evaluation.EvaluatorParser.INSTANCE.parse(pattern);
             var result = evaluator.evaluate(item);
 
