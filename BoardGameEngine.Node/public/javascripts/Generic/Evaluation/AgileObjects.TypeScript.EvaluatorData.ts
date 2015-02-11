8ca5a90042ecc9470f12e3729dfbad0660de141f
Steve Wilkes <steve@agileobjects.co.uk>
2015-02-11 11:52:23 +0000

    Evaluator parser tidying

---
+++ BoardGameEngine.Node/public/javascripts/Generic/Evaluation/AgileObjects.TypeScript.EvaluatorData.ts
@@ -0,0 +1,31 @@
+﻿module AgileObjects.TypeScript.Evaluation {
+
+    export class EvaluatorData {
+        constructor(public type: string) {
+            this.constructorArguments = new Array<any>();
+        }
+
+        public constructorArguments: Array<any>;
+        public constructorArgument: any;
+
+        public completeConstructorArgumentArray(match: RegExpExecArray, pattern: string): void {
+            if (match.index > 0) {
+                this.constructorArgument.push(pattern.substring(0, match.index));
+            }
+            var argumentArray = this.constructorArgument;
+            this.constructorArgument = null;
+            this.addConstructorArgument(argumentArray);
+        }
+
+        public completeConstructorArguments(match: RegExpExecArray, pattern: string): void {
+            if (match.index > 0) {
+                this.addConstructorArgument(pattern.substring(0, match.index));
+            }
+        }
+
+        public addConstructorArgument(value: any) {
+            ((this.constructorArgument instanceof Array)
+                ? this.constructorArgument : this.constructorArguments).push(value);
+        }
+    }
+}
--- BoardGameEngine.Node/public/javascripts/Generic/Evaluation/AgileObjects.TypeScript.EvaluatorParser.ts
+++ BoardGameEngine.Node/public/javascripts/Generic/Evaluation/AgileObjects.TypeScript.EvaluatorParser.ts
@@ -12,85 +12,53 @@
         static INSTANCE = new EvaluatorParser();
 
         public parse<T>(pattern: string): IEvaluator<T> {
-            console.log("pattern = " + pattern);
             var evaluatorSet = new EvaluatorSet<T>();
+            var evaluatorData: EvaluatorData = null;
             var match: RegExpExecArray;
-            var evaluatorType = "";
-            var constructorArguments: Array<any> = null;
-            var constructorArgument: any = null;
             while (match = _symbolMatcher.exec(pattern)) {
-                console.log("match = " + match[0] + " (" + match.index + ")");
                 switch (match[0]) {
                     case "(":
-                        console.log("Start of group");
                         match = this._getGroupEndMatch(pattern);
                         var groupPattern = pattern.substring(1, match.index + 1);
                         evaluatorSet.add(this.parse<T>(groupPattern));
                         break;
                     case ")":
-                        console.log("End of group");
                         break;
                     case "!":
                         evaluatorSet.negate = true;
-                        console.log("Negation");
                         break;
                     case "{":
-                        // start of constructor arguments
-                        evaluatorType = pattern.substring(0, match.index);
-                        constructorArguments = new Array<any>();
-                        console.log("start of constructor arguments, evaluatorType = " + evaluatorType);
+                        evaluatorData = new EvaluatorData(pattern.substring(0, match.index));
                         break;
                     case "}":
-                        // end of constructor arguments
-                        console.log("end of constructor arguments");
-                        if (match.index > 0) {
-                            constructorArgument = pattern.substring(0, match.index);
-                            constructorArguments.push(constructorArgument);
-                        }
-                        console.log("constructorArguments = " + constructorArguments.join(", "));
-                        evaluatorSet.add(_evaluatorFactories[evaluatorType].apply(null, constructorArguments));
+                        // ReSharper disable once QualifiedExpressionMaybeNull
+                        evaluatorData.completeConstructorArguments(match, pattern);
+                        var evaluatorFactory = _evaluatorFactories[evaluatorData.type];
+                        evaluatorSet.add(evaluatorFactory.apply(null, evaluatorData.constructorArguments));
                         break;
                     case "[":
-                        // start of array
-                        console.log("start of array");
-                        constructorArgument = new Array<any>();
+                        // ReSharper disable once QualifiedExpressionMaybeNull
+                        evaluatorData.constructorArgument = new Array<any>();
                         break;
                     case "]":
-                        // end of array
-                        console.log("end of array");
-                        if (match.index > 0) {
-                            // ReSharper disable once QualifiedExpressionMaybeNull
-                            constructorArgument.push(pattern.substring(0, match.index));
-                        }
                         // ReSharper disable once QualifiedExpressionMaybeNull
-                        constructorArguments.push(constructorArgument);
-                        console.log("Added array of length " + constructorArguments[constructorArguments.length - 1].length);
-                        constructorArgument = null;
+                        evaluatorData.completeConstructorArgumentArray(match, pattern);
                         break;
                     case ",":
-                        // element separator
-                        var value = pattern.substring(0, match.index);
-                        ((constructorArgument instanceof Array)
-                            ? constructorArgument : constructorArguments).push(value);
-                        console.log("element separator");
+                        // ReSharper disable once QualifiedExpressionMaybeNull
+                        evaluatorData.addConstructorArgument(pattern.substring(0, match.index));
                         break;
                     case "+":
                         evaluatorSet.setToAnd();
-                        console.log("and operator");
                         break;
                     case "|":
                         evaluatorSet.setToOr();
-                        console.log("or operator");
                         break;
                 }
                 pattern = pattern.substring(match.index + 1);
-                console.log("pattern updated to " + pattern);
             }
 
-            if (evaluatorSet.evaluator !== null) {
-                console.log("");
-                return evaluatorSet.evaluator;
-            }
+            if (evaluatorSet.evaluator !== null) { return evaluatorSet.evaluator; }
 
             throw new Error("Unable to parse Evaluator");
         }
@@ -111,9 +79,6 @@
             }
 
             throw new Error("Unable to parse group end match");
-        }
-
-        private _balls<T>(evaluator: IEvaluator<T>, negate: boolean) {
         }
     }
 }
