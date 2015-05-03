09a749202dbb2f6a9b797989ba8fae75bb359449
Steve Wilkes <steve@agileobjects.co.uk>
2015-05-03 09:13:44 +0100

    Recording History.previousAction / Tidying

--- BoardGameEngine.Node/public/javascripts/Status/History.ts
+++ BoardGameEngine.Node/public/javascripts/Status/History.ts
@@ -1,30 +1,31 @@
 ﻿module AgileObjects.BoardGameEngine.Status {
 
     export class History {
-        constructor(events: Games.GameEventSet) {
-            events.pieceMoved.subscribe(movement => this._recordPieceMovement(movement));
-            events.pieceAttacked.subscribe(attack => this._recordPieceAttack(attack));
+        private _previousMovement: P.PieceMovement;
+
+        constructor(events: G.GameEventSet) {
+            events.pieceMoved.subscribe(movement => this._recordPieceMovement(movement) === void (0));
+            events.pieceAttacked.subscribe(attack => this._record(attack) === void (0));
 
             this.actions = new Array<I.IGameAction>();
         }
 
         public actions: Array<I.IGameAction>;
-        public lastMove: Pieces.PieceMovement;
+        public previousAction: I.IGameAction;
 
-        private _recordPieceMovement(movement: P.PieceMovement): boolean {
+        private _recordPieceMovement(movement: P.PieceMovement): void {
             // BUG: Bomb movements logged as destination -> destination
-            if (this.lastMove !== undefined) {
-                this.lastMove.setWasPartOfLastMove(false);
+            if (this._previousMovement !== undefined) {
+                this._previousMovement.setWasPartOfLastMove(false);
             }
-            this.lastMove = movement;
+            this._previousMovement = movement;
             movement.setWasPartOfLastMove(true);
-            this.actions.push(movement);
-            return true;
+            this._record(movement);
         }
 
-        private _recordPieceAttack(attack: Pieces.PieceAttack): boolean {
-            this.actions.push(attack);
-            return true;
+        private _record(action: I.IGameAction): void {
+            this.previousAction = action;
+            this.actions.push(action);
         }
     }
 } 
