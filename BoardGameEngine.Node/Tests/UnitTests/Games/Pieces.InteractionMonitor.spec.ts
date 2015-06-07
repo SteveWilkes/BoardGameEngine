f5fb9f622db0be26d9472188bbc4351c5db70ef3
Steve Wilkes <steve@agileobjects.co.uk>
2015-06-07 10:26:48 +0100

    PieceInteractionMonitor tests

--- BoardGameEngine.Node/Tests/UnitTests/Games/Pieces.InteractionMonitor.spec.ts
+++ BoardGameEngine.Node/Tests/UnitTests/Games/Pieces.InteractionMonitor.spec.ts
@@ -6,7 +6,7 @@ var TsNs = Ao.TypeScript;
 describe("Game",() => {
     describe("Pieces",() => {
         describe("InteractionMonitor",() => {
-            it("Should highlight a chosen Piece on mouse down",() => {
+            it("Should highlight a Piece on held mouse down",() => {
                 var game = gameBuilder.startGame(gc => gc
                     .withAttackThenMoveTurnInteractions()
                     .withA3x3NorthSouthBoard()
@@ -19,12 +19,12 @@ describe("Game",() => {
 
                 var piece = TsNs.Joq.first<Piece>(game.teams[0].getPieces());
 
-                mouseDownOn(piece, game);
+                holdMouseDownOn(piece, monitor);
 
-                expect(highlightedPiece(monitor)).toBe(piece);
+                expect(highlightedPiece(monitor).id).toBe(piece.id);
             });
 
-            it("Should show Potential Interactions for a chosen Piece on mouse down",() => {
+            it("Should show Potential Interactions for a Piece on held mouse down",() => {
                 var game = gameBuilder.startGame(gc => gc
                     .withAttackThenMoveTurnInteractions()
                     .withA3x3NorthSouthBoard()
@@ -37,7 +37,7 @@ describe("Game",() => {
 
                 var piece = TsNs.Joq.first<Piece>(game.teams[0].getPieces());
 
-                mouseDownOn(piece, game);
+                holdMouseDownOn(piece, monitor);
 
                 var boardTiles = game.board.getTiles();
                 var moveUpOneTile = boardTiles["2x1"].potentialInteractions();
@@ -46,25 +46,62 @@ describe("Game",() => {
                 var moveRightOneTile = boardTiles["1x2"].potentialInteractions();
                 expect(moveRightOneTile.length).toBe(1);
             });
+
+            it("Should select a Piece on mouse click",() => {
+                var game = gameBuilder.startGame(gc => gc
+                    .withAttackThenMoveTurnInteractions()
+                    .withA3x3NorthSouthBoard()
+                    .withHumanLocalAndRemotePlayers()
+                    .withATeamForPlayer(1, tc => tc
+                    .withAPieceAt(["1x1"], pc => pc
+                    .withUdlrMovementBy(1))));
+
+                var monitor = setupInteractionMonitor(game);
+
+                var piece = TsNs.Joq.first<Piece>(game.teams[0].getPieces());
+
+                mouseClickOn(piece, monitor);
+
+                expect(selectedPiece(monitor).id).toBe(piece.id);
+            });
         });
     });
 });
 
+var timeoutServices = {
+    immediate: (action: () => void, delay: number) => action(),
+    never: (action: () => void, delay: number) => { }
+};
+timeoutServices.immediate["cancel"] =
+timeoutServices.never["cancel"] = () => { };
+
 function setupInteractionMonitor(game: G.Game) {
-    var immediateTimeoutService = (action: () => void, delay: number) => action();
-    immediateTimeoutService["cancel"] = () => { };
-
-    return new Bge.Pieces.PieceInteractionMonitor(immediateTimeoutService, game);
+    return new Bge.Pieces.PieceInteractionMonitor(timeoutServices.immediate, game);
 }
 
-function mouseDownOn(piece: Piece, game: G.Game) {
-    game.events.pieceSelected.publish(piece);
+function holdMouseDownOn(piece: Piece, monitor: P.PieceInteractionMonitor) {
+    setTimeoutService(monitor, timeoutServices.immediate);
+    getGame(monitor).events.pieceSelected.publish(piece);
 }
 
-function chosenPiece(monitor): Piece {
-    return monitor._currentlyChosenPiece;
+function mouseClickOn(piece: Piece, monitor: P.PieceInteractionMonitor) {
+    setTimeoutService(monitor, timeoutServices.never);
+    getGame(monitor).events.pieceSelected.publish(piece);
+    getGame(monitor).events.pieceDeselected.publish(piece);
+}
+
+function setTimeoutService(monitor, timeoutService) {
+    monitor._timeoutService = timeoutService;
+}
+
+function getGame(monitor): G.Game {
+    return monitor._game;
 }
 
 function highlightedPiece(monitor): Piece {
     return monitor._currentlyHighlightedPiece;
+}
+
+function selectedPiece(monitor): Piece {
+    return monitor._currentlySelectedPiece;
 }
