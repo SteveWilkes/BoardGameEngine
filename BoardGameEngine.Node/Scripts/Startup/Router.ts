ee7d0c333b3344c236de12e0d91b976b1e20577e
Steve Wilkes <steve@agileobjects.co.uk>
2015-06-11 20:58:17 +0100

    Adding regex to Express routes

--- BoardGameEngine.Node/Scripts/Startup/Router.ts
+++ BoardGameEngine.Node/Scripts/Startup/Router.ts
@@ -2,12 +2,15 @@
 import express = require("express");
 import bs = require("Bootstrap");
 
+var gameType = "[a-z\-0-9]+";
+var gameId = "[a-z0-9]+";
+
 class Router implements bs.IBootstrapper {
     constructor(private _routes: Object) { }
 
     public appCreated(info: bs.SystemInfo, app: express.Express): void {
-        this._setRoutes(app, "index", "/", "/game/*");
-        this._setRoutes(app, "gameIndex", "/newGame", "/loadGame/*");
+        this._setRoutes(app, "index", "/", "/game/" + gameType + "/" + gameId, "/game/" + gameId);
+        this._setRoutes(app, "gameIndex", "/newGame", "/loadGame/" + gameId);
     }
 
     private _setRoutes(app: express.Express, routeName: string, ...routes: Array<string>) {
