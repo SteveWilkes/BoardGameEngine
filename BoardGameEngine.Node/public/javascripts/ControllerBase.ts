ba4be77f72b917ab7ae016c6de146e1a96cf529b
Steve Wilkes <steve@agileobjects.co.uk>
2015-06-27 07:07:50 +0100

    Updating and styling menu

--- BoardGameEngine.Node/public/javascripts/ControllerBase.ts
+++ BoardGameEngine.Node/public/javascripts/ControllerBase.ts
@@ -4,7 +4,15 @@
     export class ControllerBase {
 
         public menu = {
+            isOpen: false,
             selected: <string>null,
+            toggleOpen: function () {
+                this.isOpen = !this.isOpen;
+                if (!this.isOpen) { this.selected = null; }
+            },
+            show: function (section: string) {
+                return (this.selected === section) || (this.isOpen && this.selected === null);
+            },
             toggle: function (section: string) {
                 this.selected = (this.selected !== section) ? section : null;
             }
