fa3670e7b00c4ef0df12e9dd860ebaecb0c1b752
Steve Wilkes <steve@agileobjects.co.uk>
2015-06-27 16:41:11 +0100

    Converting SavePlayerCommand to Node module

--- BoardGameEngine.Node/Scripts/Players/QueryObjects/SavePlayerCommand.ts
+++ BoardGameEngine.Node/Scripts/Players/QueryObjects/SavePlayerCommand.ts
@@ -1,23 +1,22 @@
-﻿module AgileObjects.BoardGameEngine.Players {
+﻿class SavePlayerCommand implements Ts.ICommand<Pl.Player> {
+    private _rootSaveDirectory: string;
 
-    export class SavePlayerCommand implements Ts.ICommand<Player> {
-        private _rootSaveDirectory: string;
+    constructor(private _fileManager: Ts.IFileManager) {
+        this._rootSaveDirectory = this._fileManager.joinPaths(
+            this._fileManager.getAppRootDirectory(),
+            "_savedData");
+    }
 
-        constructor(private _fileManager: Ts.IFileManager) {
-            this._rootSaveDirectory = this._fileManager.joinPaths(
-                this._fileManager.getAppRootDirectory(),
-                "_savedData");
-        }
+    public execute(player: Pl.Player): void {
+        var playerJson = JSON.stringify(player);
 
-        public execute(player: Player): void {
-            var playerJson = JSON.stringify(player);
+        var pathToPlayerSaveFile = this._fileManager.joinPaths(
+            this._rootSaveDirectory,
+            "player_" + player.id + ".json");
 
-            var pathToPlayerSaveFile = this._fileManager.joinPaths(
-                this._rootSaveDirectory,
-                "player_" + player.id + ".json");
-
-            this._fileManager.deleteFile(pathToPlayerSaveFile);
-            this._fileManager.writeAllText(pathToPlayerSaveFile, playerJson);
-        }
+        this._fileManager.deleteFile(pathToPlayerSaveFile);
+        this._fileManager.writeAllText(pathToPlayerSaveFile, playerJson);
     }
 }
+
+export = SavePlayerCommand;
