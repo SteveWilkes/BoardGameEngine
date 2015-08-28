7fbbf3cd59946b25ecb849222c56dc68810c9420
Steve Wilkes <steve@agileobjects.co.uk>
2015-08-28 18:51:46 -0500

    Fixing typo

--- BoardGameEngine.Node/Scripts/Games/EventHandlers/GameLoadHandler.ts
+++ BoardGameEngine.Node/Scripts/Games/EventHandlers/GameLoadHandler.ts
@@ -1,17 +1,17 @@
 ﻿class GameLoadHandler implements G.IGameSocketEventHandler {
     constructor(
         private _gameMapper: G.GameMapper,
-        private _getGetDataQuery: Ts.IGetQuery<G.GameData>) { }
+        private _getGameDataQuery: Ts.IGetQuery<G.GameData>) { }
 
     public setup(socket: G.IGameSocket): void {
         socket.on("gameLoadRequested",(request: Pl.PlayerRequest) => {
             // TODO: Validate load request
-            var gameData = this._getGetDataQuery.execute(request.gameId);
+            var gameData = this._getGameDataQuery.execute(request.gameId);
             socket.emit("gameLoadValidated", gameData);
         });
 
         socket.on("gameRestarted",(data: Pl.PlayerRequest) => {
-            var gameData = this._getGetDataQuery.execute(data.gameId);
+            var gameData = this._getGameDataQuery.execute(data.gameId);
             var game = this._gameMapper.map(gameData);
             socket.addGame(game);
             socket.emitToGameRoom("playerJoinValidated", data, game.id);
